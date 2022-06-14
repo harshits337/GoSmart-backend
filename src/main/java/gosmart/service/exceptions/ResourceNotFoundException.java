b966617a7fd79a8ff580e2c39c9a7793c4ea9a8c
Harshit <harshits337@gmail.com>
2022-06-15 01:36:06 +0530

    Inital Backend Setup

---
+++ src/main/java/gosmart/service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package gosmart.service.exceptions;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResourceNotFoundException extends RuntimeException {
+
+    private String message;
+
+    public ResourceNotFoundException(String message) {
+        this.message = message;
+    }
+}
+
